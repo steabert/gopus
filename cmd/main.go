22b7f7c6106df0817e07a25a443d4e595a4e2db1
steabert <you@example.com>
2024-09-29 10:40:47 +0200

    read entry point from command line arguments

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/user"
+)
 
+func main() {
+	flag.Parse()
+
+	var entryPoint string
+	switch flag.NArg() {
+	case 0:
+		u, err := user.Current()
+		if err != nil {
+			panic(fmt.Errorf("while getting current user: %v", err))
+		}
+		entryPoint = u.HomeDir
+	case 1:
+		entryPoint = flag.Arg(0)
+	default:
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if entryPoint == "" {
+	}
+
+	fmt.Printf("Searching for .opus files in %s\n", entryPoint)
 }
